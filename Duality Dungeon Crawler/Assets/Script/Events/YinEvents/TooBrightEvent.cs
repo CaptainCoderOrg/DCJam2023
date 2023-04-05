0ea6d391d22bf1b77756d8c178d5af37462a403b
Joseph Collard <jcollard@users.noreply.github.com>
2023-04-05 07:55:25 -0700

    chore: Change TooBrightEvent to be per step.

---
+++ Duality Dungeon Crawler/Assets/Script/Events/YinEvents/TooBrightEvent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "TooBrightEvent", menuName = "BodyMind/Events/Yin/Too Bright")]
+public class TooBrightEvent : MapEvent
+{
+    private PlayerData Player => GameManager.Instance.Player;
+
+    public override bool OnEnter()
+    {
+        if (Player.Effects.HasFlag(PlayerEffect.Shade)) { return false; }
+        MessageController.Display("There is a blinding light in this area that is overwhelming.");
+        Player.Stats.Stat(DualStat.SunMoon).Value += 10;        
+        return false;
+    }
+
+}
