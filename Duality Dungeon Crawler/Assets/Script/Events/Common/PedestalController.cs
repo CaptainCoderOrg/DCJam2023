98e1f772fc54b78d1381e285fafd6cb9df1444ab
Joseph Collard <jcollard@users.noreply.github.com>
2023-04-07 11:09:43 -0700

    chore: Add EnergyProjectile.

--- Duality Dungeon Crawler/Assets/Script/Events/Common/PedestalController.cs
+++ Duality Dungeon Crawler/Assets/Script/Events/Common/PedestalController.cs
@@ -1,16 +1,21 @@
 using CaptainCoder.Core;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class PedestalController : MonoBehaviour
 {
     public static Dictionary<Position, PedestalController> Pedestals = new();
-
+    
     public bool IsLit;
+    public bool IsFiring;
     public GameObject Center;
     public Direction Direction;
     public PositionController PositionController { get; set; }
     public GameObject FireEffect;
+    private Coroutine routine;
+    public ProjectileController EnergyBall;
+    public Transform EnergyBallSpawnPoint;
 
     public void Awake()
     {
@@ -48,6 +53,27 @@ public class PedestalController : MonoBehaviour
             IsLit = true;
             FireEffect.SetActive(true);
         }
+    }
+
+    public void Update()
+    {
+        if (IsFiring && routine == null)
+        {
+            routine = StartCoroutine(Fire());
+        }
+    }
+
+    public IEnumerator Fire()
+    {
+        ProjectileController ball = Instantiate(EnergyBall);
+        ball.transform.SetParent(PlayerMovementController.Instance.CurrentMap.transform);
+        Vector3 p = transform.position;
+        // p.y = 2.5f;
+        // p.x += 
+        ball.transform.position = p;
+        ball.Direction = Direction;
+        yield return new WaitForSeconds(1f);
+        routine = null;
     }
 
 }
