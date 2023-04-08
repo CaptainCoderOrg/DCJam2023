5d1d4a8a89a4240d01fcd65120819d4d1de09ddc
Joseph Collard <jcollard@users.noreply.github.com>
2023-04-08 00:06:24 -0700

    chore: Update title screen.

--- Duality Dungeon Crawler/Assets/UI/TitleScreenController.cs
+++ Duality Dungeon Crawler/Assets/UI/TitleScreenController.cs
@@ -1,10 +1,11 @@
+using CaptainCoder.Audio;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class TitleScreenController : MonoBehaviour
 {
-
+    public AudioClip Music;
     public OptionsMenuController Options;
     public void Awake()
     {
@@ -14,6 +15,8 @@ public class TitleScreenController : MonoBehaviour
 
         Button options = root.Q<Button>("OptionsButton");
         options.clicked += OptionsMenu;
+
+        MusicController.Instance.StartTrack(Music);
         
     }
 
